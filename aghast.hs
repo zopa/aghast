68a2f982f2448015f08701c60868f8e5990b06bb
Ben Doyle <benjamin.peter.doyle@gmail.com>
2014-02-10 14:34:00 -0500

    Marshal instance for JSAny

--- aghast.hs
+++ aghast.hs
@@ -1,29 +1,30 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Haste
 import Haste.Foreign
 import Haste.Prim
+import Unsafe.Coerce
 
 type Object a = [(JSString, a)]
 
-foreign import ccall jsSet :: Unpacked -> JSString -> JSString -> IO ()
+instance Marshal JSAny where
+    pack = toPtr . unsafeCoerce
+    unpack = unsafeCoerce . fromPtr
+    
+foreign import ccall jsSet :: JSAny -> JSString -> JSString -> IO () 
 
-phoneList :: Unpacked -> IO ()
-phoneList scope = jsSet scope (toJSString "phones") obj
-
-cameraType :: Unpacked -> IO ()
-cameraType cam = jsSet cam (toJSString "type") (toJSString "DSL")
-
+cameraType :: JSAny -> JSString -> IO ()
+cameraType cam typ = jsSet cam "type" typ
 
 prod :: Int -> IO Int
 prod n = return $ product [1 .. n]
 
-obj :: JSString
-obj = toJSString $ "[{'name': 'Nexus S', 'snippet': 'Fast, says google'},"
-                    ++ "{'name': 'XOOM', 'snippet': 'Tablet propaganda'}]"
-
 main = do
-    --export "phoneList" phoneList
     export "prod" prod
     export "cameraType" cameraType
+
--- frame.js
+++ frame.js
@@ -6,19 +6,19 @@ function product(n) {
     return p;
 }
 
+var camera = {lens: 'zoom lens', strap: 'black strap'};
+
 function jsSet(elem, prop, val) {
     elem[prop] = val;
 }
 
 
-
 function jsmain() {
     console.log('Starting test phase...');
-    var result = product(10);
-    console.log('The product up to 10 is:' + result);
-    var camera = {lens: 'zoom lens', strap: 'black strap'};
-    console.log('With Haskell: ' + Haste.prod(10));
-    Haste.cameraType(camera, null);
+    console.log(camera);
+    Haste['cameraType'](camera, 'DSL');
     console.log(camera);
     console.log('Ending test phase.');
 }
+
+// jsmain();
